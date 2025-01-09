576b47345d7b1b4f3159d16270c3a9cb0e1fddb9
Wynn @ Mon Aung <mon.aung@gmail.com>
2025-01-10 10:20:33 +1300

    split the files

--- IdentityApi/Infrastructure/Repository/IUnitOfWork.cs
+++ IdentityApi/Infrastructure/Repository/IUnitOfWork.cs
@@ -1,8 +1,4 @@
-﻿using IdentityApi.Domain;
-using IdentityApi.Infrastructure.Data;
-using Microsoft.AspNetCore.Identity;
-
-namespace IdentityApi.Infrastructure.Repository
+﻿namespace IdentityApi.Infrastructure.Repository
 {
     public interface IUnitOfWork
     {
@@ -11,39 +7,5 @@ namespace IdentityApi.Infrastructure.Repository
         IAppUser AppUser { get; }
         IClaim Claim { get; }
         Task SaveChangesAsync();
-    }
-
-    public class RepositoryManagement : IUnitOfWork
-    {
-        private readonly AppDbContext _context;
-        private readonly UserManager<AppUser> _userManager;
-        private readonly IJwtToken _jwtToken;
-        private readonly IRefreshToken _refreshToken;
-        private readonly IAppUser _appUser;
-        private readonly IClaim _claim;
-
-        public RepositoryManagement(AppDbContext context
-            , UserManager<AppUser> userManager, IConfiguration config)
-        {
-            _context = context;
-            _userManager = userManager;
-            _jwtToken = new JwtTokenManagement(config);
-            _refreshToken = new RefreshTokenManagement(_context);
-            _appUser = new AppUserManagement(_userManager);
-            _claim = new ClaimManagement(_userManager);
-        }
-        public IJwtToken JwtToken => _jwtToken;
-
-        public IRefreshToken RefreshToken => _refreshToken;
-
-        public IAppUser AppUser => _appUser;
-
-        public IClaim Claim => _claim;
-
-        public async Task SaveChangesAsync()
-        {
-            await _context.SaveChangesAsync();
-            return;
-        }
     }
 }
